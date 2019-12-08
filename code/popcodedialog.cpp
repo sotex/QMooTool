8be99e08188ed5dc465125add2e5d0905ffb290e
solym <ymwh@foxmai.com>
2019-12-08 15:11:47 +0800

    添加 PopCodeDialog 界面设计类
    Signed-off-by: solym <ymwh@foxmai.com>

---
+++ code/popcodedialog.cpp
@@ -0,0 +1,38 @@
+﻿#include "code/popcodedialog.hpp"
+#include "ui_popcodedialog.h"
+
+#include "QJSONHighlighter"
+#include "QCXXHighlighter"
+#include "QXMLHighlighter"
+
+PopCodeDialog::PopCodeDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::PopCodeDialog)
+{
+    ui->setupUi(this);
+    // ui->codeEdit->setTabReplace(true);
+    // ui->codeEdit->setTabReplaceSize(4);
+}
+
+PopCodeDialog::~PopCodeDialog()
+{
+    delete ui;
+}
+
+void PopCodeDialog::setHighlighter(const QString &langauge)
+{
+    if(0 == langauge.compare(QStringLiteral("JSON"),Qt::CaseInsensitive)){
+        ui->codeEdit->setHighlighter(new QJSONHighlighter());
+    }
+    else if(0 == langauge.compare(QStringLiteral("XML"),Qt::CaseInsensitive)){
+        ui->codeEdit->setHighlighter(new QXMLHighlighter());
+    }
+    else if(0 == langauge.compare(QStringLiteral("CXX"),Qt::CaseInsensitive)){
+        ui->codeEdit->setHighlighter(new QCXXHighlighter());
+    }
+}
+
+void PopCodeDialog::setText(const QString &text)
+{
+    ui->codeEdit->setText(text);
+}
---
+++ code/popcodedialog.hpp
@@ -0,0 +1,26 @@
+﻿#ifndef POPCODEDIALOG_HPP
+#define POPCODEDIALOG_HPP
+
+#include <QDialog>
+
+namespace Ui {
+class PopCodeDialog;
+}
+
+class PopCodeDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit PopCodeDialog(QWidget *parent = nullptr);
+    ~PopCodeDialog();
+
+    void setHighlighter(const QString& langauge);
+
+    void setText(const QString& text);
+private:
+    Ui::PopCodeDialog *ui;
+};
+Q_DECLARE_OPAQUE_POINTER(PopCodeDialog)
+
+#endif // POPCODEDIALOG_HPP
